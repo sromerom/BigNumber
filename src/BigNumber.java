5b2c179b3dbef13fcd41794cd93048afe45b4ece
Samuel Romero <sromerom@esliceu.net>
2020-01-22 12:04:45 +0100

    Passat primer test (equals and compareTo). Contemplar mes opcions mes endavant

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -49,17 +49,72 @@ public class BigNumber {
     // Compara dos BigNumber. Torna 0 si són iguals, -1 si el primer és menor
 // i torna 1 si el segon és menor
     public int compareTo(BigNumber other) {
+        String s1 = llevaZeros(this.s);
+        String s2 = llevaZeros(other.s);
+
+        if (s1.length() > s2.length()) {
+            return 1;
+        } else if (s1.length() < s2.length()) {
+            return -1;
+        } else { //Son d'igual tamany
+            for (int i = 0; i < s1.length(); i++) {
+                int actualXifraS1 = Character.getNumericValue(s1.charAt(i));
+                int actualXifraS2 = Character.getNumericValue(s2.charAt(i));
+                //System.out.println("Son iguales? " + actualXifraS1 + " --> " + actualXifraS2);
+                if (actualXifraS1 != actualXifraS2) {
+                    if (actualXifraS1 > actualXifraS2) {
+                        return 1;
+                    } else if (actualXifraS1 < actualXifraS2) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+        }
         return 0;
     }
 
     // Torna un String representant el número
     public String toString() {
-        return this.s;
+        return this.getS();
     }
 
     // Mira si dos objectes BigNumber són iguals
     public boolean equals(Object other) {
 
+        if (other instanceof BigNumber) {
+            BigNumber b = (BigNumber) other;
+            if (llevaZeros(this.s).equals(llevaZeros(b.s))) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String llevaZeros(String s) {
+
+        String res = "";
+        boolean principi = true;
+        for (int i = 0; i < s.length(); i++) {
+            int actualDigit = Character.getNumericValue(s.charAt(i));
+            //System.out.println(actualDigit);
+            if (actualDigit == 0 && principi) {
+
+            } else {
+                principi = false;
+                res = res + actualDigit;
+            }
+
+        }
+        return res;
     }
 }
