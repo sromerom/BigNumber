ee5563214f00fd2edc98cf48ec8e8b9dd140a58a
Samuel Romero <sromerom@esliceu.net>
2020-01-22 13:14:22 +0100

    Fet els tests de add

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -1,3 +1,5 @@
+import javafx.scene.effect.Blend;
+
 public class BigNumber {
     private String s;
     // Constructor 1
@@ -7,12 +9,52 @@ public class BigNumber {
 
     // Constructor 2
     public BigNumber(BigNumber b) {
-
+        this.s = b.s;
     }
 
     // Suma
     BigNumber add(BigNumber other) {
-        return new BigNumber("");
+        String res = "";
+        //StringBuilder sb = new StringBuilder();
+        String s1 = llevaZeros(this.s);
+        String s2 = llevaZeros(other.s);
+
+        //Preparam la suma
+        if (s1.length() > s2.length()) {
+            s2 = preparaSuma(s2, s1.length());
+        } else if (s1.length() < s2.length()) {
+            s1 = preparaSuma(s1, s2.length());
+        }
+
+        int aux = 0;
+        //Començam a operar
+        for (int i = 1; i < s1.length() + 1; i++) {
+            int actualXifraS1 = Character.getNumericValue(s1.charAt(s1.length() - i));
+            int actualXifraS2 = Character.getNumericValue(s2.charAt(s2.length() - i));
+            System.out.println("s1: " + actualXifraS1);
+            System.out.println("s2: " +  actualXifraS2);
+
+            //Començam a operar
+            int sumaActual = actualXifraS1 + actualXifraS2 + aux;
+            aux = 0;
+            if (sumaActual < 10) {
+                //sb.append(sumaActual);
+                res = res + sumaActual;
+            } else if (sumaActual > 9 && i < s1.length()) {
+                String seperaDigits = Integer.toString(sumaActual);
+                int desena = Character.getNumericValue(seperaDigits.charAt(0));
+                int unitat = Character.getNumericValue(seperaDigits.charAt(1));
+
+                //sb.append(unitat);
+                res = res + unitat;
+                aux = desena;
+            } else if (sumaActual > 9 && i == s1.length()) {
+                //sb.append(giraResultat(Integer.toString(sumaActual)));
+                res = res + giraResultat(Integer.toString(sumaActual));
+            }
+        }
+
+        return new BigNumber(giraResultat(res));
     }
 
     // Resta
@@ -115,6 +157,30 @@ public class BigNumber {
             }
 
         }
+        return res;
+    }
+
+    public String preparaSuma(String s, int maxim) {
+        String res = "";
+        int afegir = maxim - s.length();
+        for (int i = 0; i < afegir; i++) {
+            res = res + "0";
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            String actualXifra = Character.toString(s.charAt(i));
+            res = res + actualXifra;
+        }
+        return res;
+    }
+
+    public String giraResultat(String s) {
+        String res = "";
+        for (int i = 1; i < s.length() + 1; i++) {
+            String actualXifra = Character.toString(s.charAt(s.length() - i));
+            res = res + actualXifra;
+        }
+        System.out.println(res);
         return res;
     }
 }
