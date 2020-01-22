4ce4871d12e6b5f97c55d97162678d8ae5a3e1b8
Samuel Romero <sromerom@esliceu.net>
2020-01-22 10:45:12 +0100

    Creat esquelet inicial de la classe BigNumber

---
+++ src/BigNumber.java
@@ -0,0 +1,47 @@
+class BigNumber {
+    // Constructor 1
+    public BigNumber(String s) {
+
+    }
+
+    // Constructor 2
+    public BigNumber(BigNumber b) {
+
+    }
+
+    // Suma
+    BigNumber add(BigNumber other) {
+        return new BigNumber("");
+    }
+
+    // Resta
+    BigNumber sub(BigNumber other) {
+        return new BigNumber("");
+    }
+
+    // Multiplica
+    BigNumber mult(BigNumber other) {
+        return new BigNumber("");
+    }
+
+    // Divideix
+    BigNumber div(BigNumber other) {
+        return new BigNumber("");
+    }
+
+    // Compara dos BigNumber. Torna 0 si són iguals, -1 si el primer és menor
+// i torna 1 si el segon és menor
+    public int compareTo(BigNumber other) {
+        return 0;
+    }
+
+    // Torna un String representant el número
+    public String toString() {
+        return "";
+    }
+
+    // Mira si dos objectes BigNumber són iguals
+    public boolean equals(Object other) {
+        return false;
+    }
+}
